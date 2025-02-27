0dc8e79078dac9028715fd50137cbc10a3650785
THEGAMER221148 <weststefany@icloud.com>
2025-02-27 11:56:07 -0800

    explexleelelelellellelleleleelleleeleeeeeeee
    "werwote histroy"
    
    -Albert

---
+++ assets/js/adventureGame/computer2answers.js
@@ -0,0 +1,13 @@
+const computer2answers = [
+    "3", // option 3 for the first question
+    "8", // option 3 for the second question
+    "Octocat", // option 2 for the third question
+    "git push", // option 1 for the fourth question
+    "Have multiple people edit one file simaltaneously", // option 4 for the fifth question
+    "Code, Code, Code", // option 2 for the sixth question
+    "C", // option 3 for the seventh question
+    "Channels", // option 1 for the eighth question
+    "Huddles"  // option 4 for the ninth question
+];
+
+export default computer2answers;
